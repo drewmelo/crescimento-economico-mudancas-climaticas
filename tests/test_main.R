6ef519cebd117541951c950a6d40ec1058d10139
André <andremelopix@hotmail.com>
2024-06-05 12:04:22 -0300

    add pasta tests e test_main.R

---
+++ tests/test_main.R
@@ -0,0 +1,13 @@
+library(testthat)
+
+test_that("Teste básico para o script main.R", {
+  # Carrega o script main.R
+  source("main.R")
+
+  # Verifica se os arquivos de saída foram gerados corretamente
+  expect_true(file.exists("figuras"))
+  expect_true(file.exists("tabelas"))
+
+  # Adicione mais testes conforme necessário para verificar se os resultados
+  # produzidos pelo main.R estão corretos
+})
